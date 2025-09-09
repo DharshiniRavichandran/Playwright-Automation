6c381a6b49852a1fc6fb964e683ab376fa81b973
DharshiniRavichandran <dharshinidravichandran@gmail.com>
2025-09-09 19:12:28 +0530

    Hard, negative & Soft assertions

--- tests/Assertion.spec.js
+++ tests/Assertion.spec.js
@@ -2,8 +2,11 @@ const{test, expect} = require('@playwright/test');
 test('Inbuild locators', async ({page})=>{
     await page.goto('https://letcode.in/radio');
 
-    //toHaveURL()-It checks whether the given url is present in the webpage
+    //Hard Assertion: toHaveURL()-It checks whether the given url is present in the webpage
     await expect(page).toHaveURL('https://letcode.in/radio');
+
+     //Soft Assertion: toHaveURL()-It checks whether the given url is present in the webpage
+     //await expect.soft(page).toHaveURL('https://letcode.in/radio');
 
     //toHaveTitle()-It checks whether the given title is present in the webpage
     await expect(page).toHaveTitle('Radio Buttons | LetCode with Koushik');
@@ -38,4 +41,11 @@ test('Inbuild locators', async ({page})=>{
     const textBox = await page.locator("//input[@id='fullName']")
     textBox.fill("Dharshini");
     await expect(textBox).toHaveValue('Dharshini');
+
+    //toHaveCount()- It checks the total no of values persent in the dropdown list
+    await page.goto('https://letcode.in/dropdowns');
+    const dropdown = await page.locator("//select[@id='lang']");
+    await expect(dropdown).toHaveCount(5);
+    await expect(dropdown).not.toHaveCount(8);
+
 })
