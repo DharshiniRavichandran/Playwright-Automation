3065ce16fa6ccc2431435896bf52aef1cd11b4cc
DharshiniRavichandran <dharshinidravichandran@gmail.com>
2025-09-20 19:06:41 +0530

    Screenshot & Recording When testcase gets failed

---
+++ tests/Recording.spec.js
@@ -0,0 +1,24 @@
+//const{test, expect} = require('@playwright/test');
+import {test, expect} from '@playwright/test'
+test('Recording', async ({page})=>{
+await page.goto('https://demoblaze.com/');
+
+//click on the login button-Property
+//await page.locator('id=login2').click();
+await page.click('id=login2');
+
+//Providing username and password
+await page.fill('#loginusername', 'pavanol');
+await page.fill("input[id='loginpassword']", 'test@123');
+
+//Clicking on the login button
+await page.click("//button[normalize-space()='Log in']");
+
+//logout button presence
+const logoutlink = await page.locator("//a[normalize-space()='Log ot']"); 
+await expect(logoutlink).toBeVisible();
+
+//Closing the browser
+await page.close();
+
+})
